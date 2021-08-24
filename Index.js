37899d5730ae13ce6bf1a4984d5270e0611b27db
Prasad27K <prasadkadali279@gmail.com>
2021-08-24 16:00:40 +0530

    API Calls

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
 var connection = require('./MyConnection.js')
+var bodyParser = require('body-parser');
 var express = require('express')
 var app = express()
 
@@ -11,24 +12,26 @@ connection.connect(function(err)
 app.get('/api/syllabus/', function (req, res)
 {
 	console.log("");
-	connection.query("SELECT * FROM course", function (err, result, fields)
+	connection.query("SELECT * FROM Syllabus", function (err, result, fields)
 	{
-		res.status()
 		if (err) throw err;
+		res.status("200")
 		console.log(result);
-		res.send(result)
+		res.send(result);
+		res.end();
 	});
 });
 
 app.post('/api/syllabus/', function (req, res)
 {
-	console.log("");
-	connection.query("SELECT * FROM course", function (err, result, fields)
+	var postData  = req.body;
+	connection.query("INSERT INTO Syllabus SET ?", postData, function (err, result, fields)
 	{
-		res.status()
 		if (err) throw err;
+		res.status("201");
 		console.log(result);
 		res.send(result)
 	});
 });		
+
 app.listen(3000)
